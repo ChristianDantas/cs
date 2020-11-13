d9d892f14587645012792725b1296d5af237262d
DEV <dev@info.local>
2020-11-13 16:42:18 -0300

    terminando

---
+++ cs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace cs
+{
+    class cs
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------------");
+             Console.WriteLine("bem-vindo a seletiva de cs");
+              Console.WriteLine("-------------------------");
+               Console.WriteLine("você gostaria de participar?");
+               string resposta= Console.ReadLine();
+               if(resposta=="sim"){
+                   Console.WriteLine("que legal! agora vamos analisar seu perfil");
+                   Console.WriteLine("insira sua media: ");
+                   int media = int.Parse(Console.ReadLine());
+                   Console.WriteLine("insira suas faltas: ");
+                   int faltas =int.Parse(Console.ReadLine());
+                   if(media >50 && faltas < 10){
+                       Console.WriteLine("seja bem vindo ao time");
+                   }
+                   else{
+                       Console.WriteLine("Que triste, voce nao pode participar!");
+                   }
+               }
+               else{
+                   Console.WriteLine("tudo bem, adeus!");
+               }
+        }
+    }
+}
---
+++ exercicio 1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace exercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, sou um computador que calcula quantos meses, dias, horas e minutos que você viveu");
+            Console.WriteLine("gostaria de saber alguma dessas informaçoes?");
+            string resposta=  Console.ReadLine();
+            if (resposta== "sim"){
+                Console.WriteLine("gostaria de saber sobre seus meses dias horas e minutos?");
+                string resposta3= Console.ReadLine();
+                if(resposta3=="sim"){
+                    Console.WriteLine("quantos anos você tem? ");
+                    int idade= int.Parse(Console.ReadLine());
+                    int meses= idade * 12;
+                    int dias= idade *365;
+                    int horas= idade * 8760;
+                    int minutos= idade * 525600;
+                    Console.WriteLine("você tem " + meses + " meses, " + dias + " dias, " + horas + " horas, "+ minutos +" minutos de vida" );
+                }
+                else{
+                     Console.WriteLine("muito obrigado, irei me desligar então");
+                     
+                }
+            }
+            else{
+                Console.WriteLine("então gostaria de saber apenas quantas semanas?");
+                string resposta2=  Console.ReadLine();
+               if(resposta2=="sim"){
+                   Console.WriteLine("quantos anos você tem?");
+                   int idade2= int.Parse(Console.ReadLine());
+                   int semanas= idade2 * 52;
+                    Console.WriteLine("Você tem " + semanas +" semanas de vida");
+                    if(semanas<963){
+                        Console.WriteLine("infelizmente, você ainda nao pode dirigir");
+                        Console.WriteLine("obrigado por usar meus serviços, irei desligar agora!");
+                    }
+                    else{
+                        Console.WriteLine("parabens! você ja pode dirigir");
+                         Console.WriteLine("obrigado por usar meus serviços, irei desligar agora!");
+                    }
+                
+               }
+                else
+                {
+                       Console.WriteLine("muito obrigado, irei me desligar então");
+                }
+            }
+        }
+    }
+}
