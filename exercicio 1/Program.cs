6cd56cb4e09a67226dfae4fbad19eea48a890966
Christian <christian_d_ol9000@hotmail.com>
2020-11-13 22:30:20 -0300

    terminado

--- exercicio 1/Program.cs
+++ exercicio 1/Program.cs
@@ -6,25 +6,26 @@ namespace exercicio_1
     {
         static void Main(string[] args)
         {
+           
             Console.WriteLine("Olá, sou um computador que calcula quantos meses, dias, horas e minutos que você viveu");
             Console.WriteLine("gostaria de saber alguma dessas informaçoes?");
             string resposta=  Console.ReadLine();
             if (resposta== "sim"){
-                Console.WriteLine("gostaria de saber sobre seus meses dias horas e minutos?");
-                string resposta3= Console.ReadLine();
-                if(resposta3=="sim"){
-                    Console.WriteLine("quantos anos você tem? ");
+                  Console.WriteLine("quantos anos você tem? ");
                     int idade= int.Parse(Console.ReadLine());
                     int meses= idade * 12;
                     int dias= idade *365;
                     int horas= idade * 8760;
                     int minutos= idade * 525600;
                     Console.WriteLine("você tem " + meses + " meses, " + dias + " dias, " + horas + " horas, "+ minutos +" minutos de vida" );
-                }
-                else{
-                     Console.WriteLine("muito obrigado, irei me desligar então");
-                     
-                }
+                     if(idade<18){
+                        Console.WriteLine("infelizmente, você ainda nao pode dirigir");
+                        Console.WriteLine("obrigado por usar meus serviços, irei desligar agora!");
+                    }
+                    else{
+                        Console.WriteLine("parabens! você ja pode dirigir");
+                         Console.WriteLine("obrigado por usar meus serviços, irei desligar agora!");
+                    }
             }
             else{
                 Console.WriteLine("então gostaria de saber apenas quantas semanas?");
@@ -34,7 +35,7 @@ namespace exercicio_1
                    int idade2= int.Parse(Console.ReadLine());
                    int semanas= idade2 * 52;
                     Console.WriteLine("Você tem " + semanas +" semanas de vida");
-                    if(semanas<963){
+                    if(idade2<18){
                         Console.WriteLine("infelizmente, você ainda nao pode dirigir");
                         Console.WriteLine("obrigado por usar meus serviços, irei desligar agora!");
                     }
