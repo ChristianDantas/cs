aedb3a89cd9edc3d67b6680d35310fbac080d2d3
Christian <christian_d_ol9000@hotmail.com>
2020-11-17 16:58:06 -0300

    terminado

---
+++ repetiçao/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace repetiçao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("deseja a tabuada de qual numero?: ");
+            int resposta=int.Parse(Console.ReadLine());
+             int resultado;
+             for (int contador = 10; contador > 0; contador--)
+             {
+                 resultado= resposta * contador;
+                 Console.WriteLine($"{resposta} x {contador} = {resultado}");
+             }
+        }
+    }
+}
